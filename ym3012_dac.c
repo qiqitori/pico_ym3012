228da90034ce986ac93b5df47b15a56197ed488e
sneep <sneep@example.com>
2023-03-09 13:34:25 +0900

    Now works with real YM2151 and JT51
    Add #ifdefs to allow to choose between YM2151 and JT51. (PIO program
    needs to be switched manually!)
    Still only mono output.
    
    See https://blog.qiqitori.com/?p=1082 for more information.

--- ym3012_dac.c
+++ ym3012_dac.c
@@ -1,9 +1,3 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include <stdio.h>
 
 #include "pico/stdlib.h"
@@ -17,41 +11,43 @@
 #define PIN_BASE 0
 #define AUDIO_PIN 28
 
-#define PWM_WRAP 50
+// #define DEBUG 1
+// #define JT51 1
 
-// uint16_t samples[100000] = { 0 };
+#ifdef JT51
+#define DESIRED_SAMPLE_RATE 62000 // 4 MHz VGM
+#else
+#define DESIRED_SAMPLE_RATE 57000 // 315/88 MHz / 2 / 32
+#endif
 
+uint16_t samples[110000] = { 0 };
 uint16_t last_sample;
 
-void pwm_interrupt_handler() {
-    pwm_clear_irq(pwm_gpio_to_slice_num(AUDIO_PIN));
-    pwm_set_gpio_level(AUDIO_PIN, last_sample);
-}
-
 int main() {
+#ifdef DEBUG
     stdio_init_all();
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-
     sleep_ms(5000);
     printf("Hello world\n");
+#endif
 
     // Init PWM for audio out
     gpio_set_function(AUDIO_PIN, GPIO_FUNC_PWM);
     int audio_pin_slice = pwm_gpio_to_slice_num(AUDIO_PIN);
 
-    // Setup PWM interrupt to fire when PWM cycle is complete
-//     pwm_clear_irq(audio_pin_slice);
-//     pwm_set_irq_enabled(audio_pin_slice, true);
-    // set the handle function above
-//     irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler);
-//     irq_set_enabled(PWM_IRQ_WRAP, true);
-
     // Setup PWM for audio output
+    // We run at around 125 MHz. If we set the pwm counter's top value (== wrap value) to 8192 (generally, bigger is better), the pwm counter can reach the top value 15258.7890625 times per second, which would be our effective sample rate. (Calculation: 125000000/8192)
+    // However, our target sample rate is larger than that. Let's say if we wanted 44100 Hz: 125000000/44100 = 2834.46712018, so that's the max top value we should set.
+    // However, our target sample rate is even larger than that. Let's say we want 60 KHz. Then the max top value is 2083.33333333.
+    // In that case, our samples' max loudness should be about half that, 1041.66666667.
+    // That's pretty close to 1024. That's good.
+    // Let's not hard-code this but calculate based on the desired sample rate.
+    // Note that the desired sample rate depends on the VGM tune played.
+    uint16_t pwm_wrap = clock_get_hz(clk_sys)/DESIRED_SAMPLE_RATE-24; // TODO: Check if -24 actually improves anything (original intent is to buy microcontroller some time to move to the next sample -- if we don't have enough time, pwm_set_gpio_level might not make it in time and the entire next PWM cycle would be played using the level of the previous sample. I think so anyway.)
     pwm_config config = pwm_get_default_config();
-    pwm_config_set_clkdiv(&config, 8.0f);
-    pwm_config_set_wrap(&config, PWM_WRAP);
+    pwm_config_set_clkdiv(&config, 1.0f);
+    pwm_config_set_wrap(&config, pwm_wrap);
     pwm_set_gpio_level(AUDIO_PIN, 0);
+//     pwm_set_phase_correct(audio_pin_slice, true); // TODO: maybe test if this changes anything?
     pwm_init(audio_pin_slice, &config, true);
 
     // Init state machine for PIO
@@ -60,15 +56,22 @@ int main() {
     uint offset = pio_add_program(pio, &ym3012_dac_program);
     ym3012_dac_init(pio, sm, offset, PIN_BASE);
 
-//     for (int i = 0; i < 100000; i++) {
-//         samples[i] = ym3012_dac_get_sample(pio, sm) * (16384/1024); // 16384 is 2^16 (because we're using a uint16_t), 1024 is 2^10 (because it's a 10-bit DAC)
-//     }
-//     for (int i = 0; i < 100000; i++) {
-//         printf("%d\n", samples[i]);
-//     }
+#ifdef DEBUG
+    for (int j = 0; j < 15; j++) {
+        for (int i = 0; i < 110000; i++) {
+            samples[i] = ym3012_dac_get_sample(pio, sm);
+        }
+        for (int i = 0; i < 110000; i+=8) {
+            printf("%04x %04x %04x %04x %04x %04x %04x %04x\n", samples[i], samples[i+1], samples[i+2], samples[i+3], samples[i+4], samples[i+5], samples[i+6], samples[i+7]);
+        }
+    }
+#else
     while (true) {
-        last_sample = ym3012_dac_get_sample(pio, sm) * 4; //(16384/1024); // 16384 is 2^16 (because we're using a uint16_t), 1024 is 2^10 (because it's a 10-bit DAC)
-//         printf("%d\n", last_sample);
+        last_sample = ym3012_dac_get_sample(pio, sm); // same as above
+//         printf("%04x\n", last_sample);
+        last_sample = last_sample >> 5;
+
         pwm_set_gpio_level(AUDIO_PIN, last_sample);
     }
+#endif
 }
